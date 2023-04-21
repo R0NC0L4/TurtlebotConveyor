3770894e303ec9de066d00e01cef6ad3b106bf8a
Andrea <andreamiele120@gmail.com>
2023-04-21 20:12:56 +0200

    update lidar

--- conveyor_control/scripts/roomba.cpp
+++ conveyor_control/scripts/roomba.cpp
@@ -1,4 +1,4 @@
-// This program makes the robot move forward. If the robot sees an obstacles it will rotate.
+// This program makes the robot move forward. If the robot sees an obstacles it will change direction.
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
 #include <stdlib.h>
@@ -9,40 +9,56 @@
 
 ros::Publisher pub;
 
-int getCollision(const sensor_msgs::LaserScan msg, int window, float distance)
+class Listener
+{
+public:
+   int changed = 0;
+   float prev_angle = 0;
+
+   void subCallback(const sensor_msgs::LaserScan msg);
+
+   bool getCollision(const sensor_msgs::LaserScan msg, float distance);
+};
+
+bool Listener::getCollision(const sensor_msgs::LaserScan msg, float distance)
 {
    int c = 0;
+
    for (int i = 0; i < msg.ranges.size(); i++)
    {
-      c = c || (msg.ranges[i] <= distance);
+      c = c || (msg.ranges[i] <= distance && msg.ranges[i] > 0.11);
    }
    return c;
 }
 
-void subCallback(const sensor_msgs::LaserScan msg)
+void Listener::subCallback(const sensor_msgs::LaserScan msg)
 {
-   //int collision = getCollision(msg, 700, 50);
-   bool collision = msg.range_min <= 0.5 && msg.range_min >= 0.05;
-   
+   bool collision = getCollision(msg, 0.3);
+
    conveyor_description_pkg::desired_conf state;
    float curr_vel = 10;
-   float curr_angle = state.angle;
+   float curr_angle = prev_angle;
 
-   if (collision == false)
+   if (changed != 1)
    {
-   state.velocity = curr_vel;
-   state.angle = curr_angle;
+      if (collision == false)
+      {
+         state.velocity = curr_vel;
+         state.angle = curr_angle;
+      }
+      else
+      {
+         changed = 1;
+         state.velocity = curr_vel;
+         int random_num = rand() % 100000;
+         float change = 90 + ((double)random_num / 100000) * (180 - 90); // VEDI QUESTO PROBABILE ERRORE
+         state.angle = (fmod(curr_angle + change, 360) - 180);
+         ROS_INFO("Collision. ANGLE: %f", state.angle);
+      }
+      prev_angle = state.angle;
+      state.rotate = false;
+      pub.publish(state);
    }
-   else
-   {
-      state.velocity = curr_vel;
-      // int random_num = rand() % 100000;
-      // float change = M_PI / 2 + ((double)random_num / 100000) * (M_PI - M_PI / 2);
-      // state.angle = (fmod(curr_angle + change + M_PI, 2*M_PI) - M_PI)*180/3.1459;
-      state.angle = 90;
-   }
-   state.rotate = false;
-   pub.publish(state);
 }
 
 int main(int argc, char **argv)
@@ -55,13 +71,21 @@ int main(int argc, char **argv)
    pub = nh.advertise<conveyor_description_pkg::desired_conf>("desired_configuration", 1000);
 
    // Create a subscriber object for scanner data.
-   ros::Subscriber sub = nh.subscribe("scan", 1000, subCallback);
+   Listener listener;
+   ros::Subscriber sub = nh.subscribe("scan", 1, &Listener::subCallback, &listener);
 
-   ros::Rate rate(2);
+   ros::Rate rate(0.5);
+
+   srand(time(NULL));
 
    while (ros::ok())
    {
-      ros::spin();
-      // rate.sleep();
+      ros::spinOnce();
+      // ROS_INFO("%d", listener.changed);
+      if (listener.changed == 1)
+      {
+         rate.sleep();
+         listener.changed = 0;
+      }
    }
 }
